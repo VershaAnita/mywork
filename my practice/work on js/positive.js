5dc74e8767763a78664584414a25c37216399994
VershaAnita <aversha.kumari@gmail.com>
2022-06-25 11:20:22 +0530

    work

---
+++ my practice/work on js/positive.js
@@ -0,0 +1,11 @@
+const number(4);
+const number=(prompt("Enter a number:"));
+//chek if number is greater than 0
+if (number>0){
+    console.log("The number is positive");
+ } 
+ else if (number=0){
+    console.log("The number is zero");
+}else{
+    console.log("The number is negative");
+}
